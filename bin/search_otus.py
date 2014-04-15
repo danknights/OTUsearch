5d2c6b15c15a008eea91cdeba039bdfcabb2f688
Dan Knights <danknights@gmail.com>
2014-04-14 20:21:15 -0500

    functionality now working from lib/otusearch/__init__.py

--- bin/search_otus.py
+++ bin/search_otus.py
@@ -1,90 +1,73 @@
 # python matrix_seq.py query.fasta ref.fasta .97
+import sys
+from otusearch import alignmentToBinaryMatrix
 import numpy as np
 from cogent import LoadSeqs
 import pickle
 import sys
 import time
+import optparse
 
-prevtime = time.clock()
-print "Load query seqs..."
-query = LoadSeqs(sys.argv[1])
-print time.clock() - prevtime
-prevtime = time.clock()
-print "Load ref seqs..."
-ref = LoadSeqs(sys.argv[2])
-print time.clock() - prevtime
-prevtime = time.clock()
-threshold = float(sys.argv[3])
+def get_opts():
+    p = optparse.OptionParser()
+    p.add_option("-r", "--ref_fp", type="string", \
+        default=None, help="Reference alignment file [required].")
+    p.add_option("-q", "--query_fp", type="string", \
+        default=None, help="Query alignment file [required].")
+    p.add_option("-s", "--similarity", type="float", \
+        default=.97, help="Minimum similarity [default %default]")
+    p.add_option("-o", "--output", type="string", \
+        default="otusearch.txt", help="Output results file [default %default].")
+    p.add_option("--verbose", action="store_true", \
+        help="Print all output.")
+    opts, args = p.parse_args(sys.argv)
 
-nref = len(ref.Names)
-nquery = len(query.Names)
-npos = len(ref)
+    return opts, args
 
-print "Initialize matrices..."
-dna = ['A','C','G','T','-']
-refmat = {}
-querymat = {}
-for letter in dna:
-	refmat[letter] = np.zeros((nref, npos),dtype='float32')
-	querymat[letter] = np.zeros((nquery, npos),dtype='float32')
-print time.clock() - prevtime
-prevtime = time.clock()
+def check_opts(opts):
+	if opts.ref_fp is None:
+		raise ValueError('\n\nPlease include an input reference alignment.')
+	if opts.query_fp is None:
+		raise ValueError('\n\nPlease include a input query alignment.')
 
-do_pickle = True
-# do_pickle = False
-if do_pickle:
-	print 'Build ref matrix...'
-	count = 0
-	for pos in ref.Positions:
-		pos = np.array(pos)
-		posn = pos == 'N'
-		for letter in dna:
-			refmat[letter][:,count] = np.logical_or(pos == letter, posn)
-		count += 1
-	del ref
+if __name__ == '__main__':
+	opts, args = get_opts()
+	check_opts(opts)
 
-# 	output = open('data.pkl', 'wb')
-# 	pickle.dump(refmat, output)
-# 	output.close()
-else:
-	pkl_file = open('data.pkl', 'rb')
-	refmat = pickle.load(pkl_file)
-	pkl_file.close()
+	if opts.verbose:
+		print "Initialize matrices..."
+	refmat = alignmentToBinaryMatrix(opts.ref_fp)
+	querymat = alignmentToBinaryMatrix(opts.query_fp)
+	threshold = float(opts.similarity)
 
-print time.clock() - prevtime
-prevtime = time.clock()
+	nquery = querymat[querymat.keys()[0]].shape[0]
+	nref = refmat[refmat.keys()[0]].shape[0]
+	npos = refmat[refmat.keys()[0]].shape[1]
+	
+	sim = np.zeros((nquery, nref), dtype='float32')
 
-print 'Build query matrix...'
-count = 0
-for pos in query.Positions:
-	pos = np.array(pos)
-	posn = pos == 'N'
-	for letter in dna:
-		querymat[letter][:,count] = np.logical_or(pos == letter, posn)
-	count += 1
-del query
-print time.clock() - prevtime
-prevtime = time.clock()
+	if opts.verbose:
+		print 'Matrix multiplication...'
+		prevtime = time.clock()
+	for letter in list('ACTG'):
+		sim += np.dot(querymat[letter], refmat[letter].T)
+	if opts.verbose:
+		print time.clock() - prevtime
+		prevtime = time.clock()
 
-sim = np.zeros((nquery, nref), dtype='float32')
+	# sim = sim / npos
+	if opts.verbose:
+		print 'Tally...'
+	sim_scores = np.zeros(nquery)
+	match_ix = np.zeros(nquery)
 
-print 'Matrix multiplication...'
-for letter in dna[:4]:
-	sim += np.dot(querymat[letter], refmat[letter].T)
-print time.clock() - prevtime
-prevtime = time.clock()
+	for i in xrange(nquery):
+		match_ix[i] = sim[i,:].argmax()
+		sim_scores[i] = sim[i,match_ix[i]]
+	sim_scores = sim_scores / (npos - querymat['-'].sum(1))
 
-# sim = sim / npos
-print 'Tally...'
-sim_scores = np.zeros(nquery)
-match_ix = np.zeros(nquery)
+	if opts.verbose:
+		print time.clock() - prevtime
+		prevtime = time.clock()
 
-for i in xrange(nquery):
-	match_ix[i] = sim[i,:].argmax()
-	sim_scores[i] = sim[i,match_ix[i]]
-sim_scores = sim_scores / (npos - querymat['-'].sum(1))
-print time.clock() - prevtime
-prevtime = time.clock()
-
-print sorted(sim_scores)
-print sum(sim_scores >= threshold)
+	print sum(sim_scores >= threshold)
